4a10c505e9e57c46776e66008ed149f643641f16
RCarpus <40838987+RCarpus@users.noreply.github.com>
2021-11-08 09:46:16 -0500

    replaced script with short list of pokemon objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,33 @@
-alert('hello world');
-favoriteFood = "tacos al pastor";
-document.write(favoriteFood);
+let pokemonList = [];
+let minccino = {
+    name : "Minccino",
+    height: 16,
+    type: ['normal'],
+    cutenessLevel: 'overload'
+};
+
+let dunsparce = {
+    name: "Dunsparce",
+    height: 59,
+    type: ['normal'],
+    cutenessLevel: 'pretty uggo'
+};
+
+let cramorant = {
+    name: "Cramorant",
+    height: 31,
+    type: ['flying', 'water'],
+    cutenessLevel: 'moderate'
+};
+
+let bidoof = {
+    name: "Bidoof",
+    height: 20,
+    type: ['normal'],
+    cutenessLevel: 'simultaneously the ugliest and most beautiful creature I have ever seen'
+};
+
+pokemonList.push(minccino);
+pokemonList.push(dunsparce);
+pokemonList.push(cramorant);
+pokemonList.push(bidoof);
