d491cb3c5ad2b47654ed06ce03bf0f2faca9ce96
RCarpus <40838987+RCarpus@users.noreply.github.com>
2021-11-08 13:15:37 -0500

    refactor, includes wrapping pokemonList in an IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,41 +1,59 @@
-let pokemonList = [];
-let minccino = {
-    name : 'Minccino',
-    height: 16,
-    type: ['normal'],
-    cutenessLevel: 'cuteness overload'
-};
+let pokemonRepository = (function () {
+    let pokemonList = [];
 
-let dunsparce = {
-    name: 'Dunsparce',
-    height: 59,
-    type: ['normal'],
-    cutenessLevel: 'pretty uggo'
-};
+    function add(pokemon) {
+        pokemonList.push(pokemon);
+    }
 
-let cramorant = {
-    name: 'Cramorant',
-    height: 31,
-    type: ['flying', 'water'],
-    cutenessLevel: 'moderately cute'
-};
+    function getAll() {
+        return pokemonList;
+    }
 
-let bidoof = {
-    name: 'Bidoof',
-    height: 20,
-    type: ['normal'],
-    cutenessLevel: 'simultaneously the ugliest and most beautiful creature I have ever seen'
-};
+    //example data to be replaced by real data later
+    let minccino = {
+        name : 'Minccino',
+        height: 16,
+        type: ['normal'],
+        cutenessLevel: 'cuteness overload'
+    };
+    
+    let dunsparce = {
+        name: 'Dunsparce',
+        height: 59,
+        type: ['normal'],
+        cutenessLevel: 'pretty uggo'
+    };
+    
+    let cramorant = {
+        name: 'Cramorant',
+        height: 31,
+        type: ['flying', 'water'],
+        cutenessLevel: 'moderately cute'
+    };
+    
+    let bidoof = {
+        name: 'Bidoof',
+        height: 20,
+        type: ['normal'],
+        cutenessLevel: 'simultaneously the ugliest and most beautiful creature I have ever seen'
+    };
 
-pokemonList.push(minccino);
-pokemonList.push(dunsparce);
-pokemonList.push(cramorant);
-pokemonList.push(bidoof);
+    //load in example data
+    add(minccino);
+    add(dunsparce);
+    add(cramorant);
+    add(bidoof);
+
+    return {
+        add: add,
+        getAll: getAll
+    };
+})();
 
 let largeHeight = 48;
 let smallHeight = 20;
 //Write each pokemon's name and height to the page
-pokemonList.forEach(function(pokemon) {
+pokemonRepository.getAll().forEach(function(pokemon) {
     document.write('<p>');
     document.write(`<strong>${pokemon.name}</strong> \(height: ${pokemon.height} inches\)`);
     //Print "Wow, that's big!" if the pokemon is tall and mentions cuteness level of pokemon
