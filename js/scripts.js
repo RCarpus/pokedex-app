30f01deb2052c11002711a543e5ba94016e6cada
RCarpus <40838987+RCarpus@users.noreply.github.com>
2021-11-08 10:57:46 -0500

    updated script to write pokemon data to the page

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,27 @@
 let pokemonList = [];
 let minccino = {
-    name : "Minccino",
+    name : 'Minccino',
     height: 16,
     type: ['normal'],
-    cutenessLevel: 'overload'
+    cutenessLevel: 'cuteness overload'
 };
 
 let dunsparce = {
-    name: "Dunsparce",
+    name: 'Dunsparce',
     height: 59,
     type: ['normal'],
     cutenessLevel: 'pretty uggo'
 };
 
 let cramorant = {
-    name: "Cramorant",
+    name: 'Cramorant',
     height: 31,
     type: ['flying', 'water'],
-    cutenessLevel: 'moderate'
+    cutenessLevel: 'moderately cute'
 };
 
 let bidoof = {
-    name: "Bidoof",
+    name: 'Bidoof',
     height: 20,
     type: ['normal'],
     cutenessLevel: 'simultaneously the ugliest and most beautiful creature I have ever seen'
@@ -31,3 +31,19 @@ pokemonList.push(minccino);
 pokemonList.push(dunsparce);
 pokemonList.push(cramorant);
 pokemonList.push(bidoof);
+
+let largeHeight = 48;
+let smallHeight = 20;
+//Write each pokemon's name and height to the page
+for (let i=0; i<pokemonList.length; i++) {
+    document.write('<p>');
+    document.write(`<strong>${pokemonList[i].name}</strong> \(height: ${pokemonList[i].height} inches\)`);
+    //Print "Wow, that's big!" if the pokemon is tall and mentions cuteness level of pokemon
+    if (pokemonList[i].height >= largeHeight) {
+        document.write(` Wow, that\'s big, and ${pokemonList[i].cutenessLevel}!`);
+    //Print "Wow, that's small!" if the pokemon is short and mentions cuteness level of pokemon
+    } else if (pokemonList[i].height <= smallHeight) {
+        document.write(` Wow, that\'s small, and ${pokemonList[i].cutenessLevel}!`)
+    }
+    document.write('</p>');
+}
